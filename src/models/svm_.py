210df47223dcfe6bc67727f045ef3c6125022799
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2023-03-22 15:00:29 +0100

    experiments: svm.SVR model

---
+++ src/models/svm_.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from src.preprocessing.wrangle import wrangle
+from sklearn.svm import SVR
+
+x, y = wrangle()
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8)
+
+x_scaler = preprocessing.MinMaxScaler().fit(x_train)
+x_train = x_scaler.transform(x_train)
+x_test = x_scaler.transform(x_test)
+
+y_train = np.log(y_train)
+
+
+model = SVR(C=50.0, degree=10)
+model.fit(x_train, y_train)
+pred = model.predict(x_train)
+
+
+pred = np.exp(pred)
+
+diff = abs(pred - y_train)
+print('Acceptable results percentage:', 100*sum(diff < 100)/len(diff))
