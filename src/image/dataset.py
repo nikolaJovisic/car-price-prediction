8e7cba97298400ac40fbd9bdd149d0ec2b700f0b
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2023-03-25 12:25:11 +0100

    experiments: color reading in dataset

--- src/image/dataset.py
+++ src/image/dataset.py
@@ -1,19 +1,26 @@
 import json
 import os
+
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
-def _read_labels():
-    labels = []
-    for filename in os.scandir("../../data/textual"):
-        with open(filename) as file:
+def _read_data():
+    ROOT = "../../data"
+    IMAGES = os.path.join(ROOT, 'images')
+    TEXTUAL = os.path.join(ROOT, 'textual')
+    image_paths, labels = [], []
+    for path in os.scandir(TEXTUAL):
+        with open(path) as file:
             datapoint = json.load(file)
-            labels.append(datapoint["cena"])
-    return labels
+            _, filename = os.path.split(path)
+            number = filename.split('.')[0]
+            image_paths.append(os.path.join(IMAGES, number))
+            labels.append(datapoint["dodatne informacije"]["boja"])
+    return image_paths, labels
 
 class ImageDataset(Dataset):
     def __init__(self, transform=None, target_transform=None):
-        self.img_labels = _read_labels()
+        self.img_paths, self.img_labels = _read_data()
         self.transform = transform
         self.target_transform = target_transform
 
@@ -21,8 +28,7 @@ class ImageDataset(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join("../../data/images", str(idx + 1), "1.png")
-        image = read_image(img_path)
+        image = read_image(os.path.join(self.img_paths[idx], '1.png'))
         label = self.img_labels[idx]
         if self.transform:
             image = self.transform(image)
