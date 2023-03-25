d5952617329fcb617ace1884401883911537db19
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2023-03-25 13:23:07 +0100

    experiments: one hot encoding/decoding

--- src/image/dataset.py
+++ src/image/dataset.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
@@ -21,6 +22,8 @@ def _read_data():
 class ImageDataset(Dataset):
     def __init__(self, transform=None, target_transform=None):
         self.img_paths, self.img_labels = _read_data()
+        self.vocab = list(set(self.img_labels))
+        self.vocab_size = len(self.vocab)
         self.transform = transform
         self.target_transform = target_transform
 
@@ -29,9 +32,19 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, idx):
         image = read_image(os.path.join(self.img_paths[idx], '1.png'))
-        label = self.img_labels[idx]
+        label = self.encode(self.img_labels[idx])
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
+
+    def encode(self, word):
+        one_hot = torch.zeros(self.vocab_size)
+        one_hot[self.vocab.index(word)] = 1
+        return one_hot
+
+    def decode(self, one_hot):
+        one_hot = torch.squeeze(one_hot)
+        index = torch.where(one_hot == 1)[0]
+        return self.vocab[index]
