136145d5144d734fbf09a8acd36291634c180c08
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2023-04-01 13:18:13 +0200

    experiments: architectural changes, intermediate results map

--- src/image/color_classifier/net.py
+++ src/image/color_classifier/net.py
@@ -1,23 +1,38 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from torch import squeeze, transpose
 
 
 class ColorClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(4, 4)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(4624, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 18)
+        self.conv = nn.Conv2d(3, 1, 5)
+        self.pool = nn.MaxPool2d(4, 6)
+        self.fc = nn.Linear(16, 18)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        image_disp = squeeze(x[0])
+        image_disp = transpose(image_disp, 0, 2)
+        plt.imshow(image_disp)
+        plt.show()
+
+        x = F.relu(self.conv(x))
+
+        image_disp = x[0].detach().numpy()
+        image_disp = np.transpose(image_disp, [1, 2, 0])
+        plt.imshow(image_disp)
+        plt.show()
+
+        x = self.pool(x)
+
+        image_disp = x[0].detach().numpy()
+        image_disp = np.transpose(image_disp, [1, 2, 0])
+        plt.imshow(image_disp)
+        plt.show()
+
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc(x))
         return x
