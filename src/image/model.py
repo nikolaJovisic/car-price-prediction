314fce05894b6157c454a80461c44183509a6066
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2023-04-06 13:57:47 +0200

    experiments: update custom architecture

--- src/image/model.py
+++ src/image/model.py
@@ -7,15 +7,19 @@ from torchvision.models import resnet18
 class ColorClassifier(nn.Module):
     def __init__(self, outputs):
         super().__init__()
-        self.conv = nn.Conv2d(3, 3, 5)
+        self.conv1 = nn.Conv2d(3, 3, 3)
+        self.conv2 = nn.Conv2d(3, 3, 3)
         self.pool = nn.MaxPool2d(6, 6)
-        self.fc = nn.Linear(48, outputs)
+        self.fc1 = nn.Linear(27, 24)
+        self.fc2 = nn.Linear(24, outputs)
 
     def forward(self, x):
-        x = F.relu(self.conv(x))
+        x = F.relu(self.conv1(x))
         x = self.pool(x)
+        x = F.relu(self.conv2(x))
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc(x))
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
 def get_resnet18(outputs_size):
@@ -31,5 +35,5 @@ def get_resnet18(outputs_size):
     return model
 
 def get_model(outputs_size):
-    #return ColorClassifier(outputs_size)
+    # return ColorClassifier(outputs_size)
     return get_resnet18(outputs_size)
